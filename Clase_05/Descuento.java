d13d31684f1ddce22580045ee98c5088adf043d4
Hector <hector_ing2018@hotmail.com>
2023-04-11 20:35:05 -0300

    '..'

---
+++ Clase_05/Descuento.java
@@ -0,0 +1,8 @@
+package Clase_05;
+
+public class Descuento {
+
+    public void desc() {
+
+    }
+}
--- Clase_05/Producto.java
+++ Clase_05/Producto.java
@@ -1,5 +1,37 @@
 package Clase_05;
 
 public class Producto {
-    
+    private String nombre;
+    private int cantidad;
+    private double precio;
+
+    public Producto(String nombre, int cantidad, double precio) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
 }
