7b39b5c5a56e8da0b15d43c131d6a5017630e3b8
Hector <hector_ing2018@hotmail.com>
2023-04-12 19:46:12 -0300

    '..'

--- Clase_05/Carrito.java
+++ Clase_05/Carrito.java
@@ -1,5 +1,13 @@
 package Clase_05;
 
+import java.time.LocalTime;
+import java.util.*;
+
+import Integrador.main;
+
 public class Carrito {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
 }
